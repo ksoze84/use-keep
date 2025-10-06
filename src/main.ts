354813bce8af22146bb478fe798e5d78fb7978ec
Felipe Rodriguez Herrera <felipe.rodriguez@epysa.cl>
2025-10-05 21:39:55 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,47 @@
+import { useDebugValue, useSyncExternalStore } from "react";
+
+const notProvided = Symbol("notProvided");
+
+type KeepType<T> = {
+  (s : T|((s:T) => T) ): undefined;
+  (): T;
+  subscribe: (listener: () => void) => () => void;
+  assign: <L extends KeepType<T>, S>(this: L, k: S) => L & S;
+  use: () => T;
+}
+
+export function keep<T>( initialState: T ): KeepType<T>;
+export function keep<T,S>( initialState: T, k: S ): KeepType<T> & S;
+export function keep<T,S>( initialState: T, k?: S ){
+  const listeners = new Set<() => void>();
+  const subscribe = (listener: () => void) => {
+    listeners.add(listener);
+    return () => listeners.delete(listener);
+  }
+  const state = (( s : T|((s:T) => T) = notProvided as T) => {
+    if(s !== notProvided){ 
+      initialState = typeof s === 'function' ? (s as (s:T) => T)(initialState) : s;
+      for (const listener of listeners) listener();
+    }
+    else
+      return initialState;
+  }) as KeepType<T>;
+  state.subscribe = subscribe;
+  state.use = () => useKeep(state);
+  state.assign = function(this, k) {
+    return Object.assign(this, k);
+  }
+  if (k)
+    return state.assign(k);
+  return state;
+}
+
+export function useKeep<T, K>( store: ReturnType<typeof keep<T, K>> ) : T {
+  const state = useSyncExternalStore(
+    store.subscribe,
+    store,
+    store // For server-side rendering, return the current value
+  );
+  useDebugValue(state);
+  return state;
+}
